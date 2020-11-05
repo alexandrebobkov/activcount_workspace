7162360775e4861edab422b464da09e782c18dd2
Alexandre B <alexandre.bobkov@gmail.com>
2020-11-04 19:04:40 -0500

    2

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
 
+import 'login_page.dart';
+
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.blue[100]),
+      body: Container(color: Colors.green[100],
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              BizLogo(),
+              _signOutButton(),
+            ],
+          ),
+
+        ),
+      ),
     );
   }
+
+  Widget _signOutButton() {
+    return OutlineButton(
+      splashColor: Colors.red,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      borderSide: BorderSide(color: Colors.red),
+      onPressed: () {
+        //Navigator.of(context).push(
+          MaterialPageRoute(
+            // FIX: return to first screen when clicked + logout
+            builder: (context) {
+              return FirstScreen();
+            },
+          );
+      },
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/google_logo.png"), height: 40.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign Out',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.green,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+Widget BizLogo() {
+  return Image(image: AssetImage("assets/google_logo.png"), height: 200);
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:activcount_workspace/sign_in.dart';
 import 'package:flutter/material.dart';
 
+import 'first_screen.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -11,14 +13,15 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.white,
+        color: Colors.white60,
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlutterLogo(size: 150),
-              SizedBox(height: 60),
+              BizLogo(),
+              //FlutterLogo(size: 150),
+              //SizedBox(height: 60),
               _signInButton(),
             ],
           ),
@@ -27,38 +30,40 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+
   Widget _signInButton() {
     return OutlineButton(
-      splashColor: Colors.grey,
-      onPressed: () {signInWithGoogle().then((result) {
-        if (result != null) {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) {
-                return FirstScreen();
-              },
-            ),
-          );
-        }
-      });
+      splashColor: Colors.green,
+      onPressed: () {
+        signInWithGoogle().then((result) {
+          if (result != null) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return FirstScreen();
+                },
+              ),
+            );
+          }
+        });
       },
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       highlightElevation: 0,
-      borderSide: BorderSide(color: Colors.grey),
+      borderSide: BorderSide(color: Colors.red),
       child: Padding(
         padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
         child: Row(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Image(image: AssetImage("assets/google_logo.png"), height: 40.0),
             Padding(
               padding: const EdgeInsets.only(left: 10),
               child: Text(
                 'Sign in with Google',
                 style: TextStyle(
                   fontSize: 20,
-                  color: Colors.grey,
+                  color: Colors.green,
                 ),
               ),
             )
@@ -69,11 +74,16 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-class FirstScreen extends StatelessWidget {
+
+Widget BizLogo() {
+    return Image(image: AssetImage("assets/google_logo.png"), height: 200);
+}
+
+/*class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(color: Colors.blue[100]),
     );
   }
-}
+}*/
